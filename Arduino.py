a2bd9460ff971f003c240df80e485ca370b6270a
KIM2C1 <119794073+KIM2C1@users.noreply.github.com>
2023-02-12 14:19:25 +0900

    .

---
+++ Arduino.py
@@ -0,0 +1,11 @@
+import serial
+import time
+
+ser = serial.Serial("COM9", 9600) # 아두이노의 포트와 보율을 지정합니다.
+c = input()
+c = c.encode('utf-8')
+ser.write(c)
+
+time.sleep(1)
+y = ser.readline()
+print(y.decode('utf-8').strip())
