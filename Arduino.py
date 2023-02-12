56e4d4f8a21af82b5d3e8213f2cd2bd955fcd111
KIM2C1 <119794073+KIM2C1@users.noreply.github.com>
2023-02-12 14:53:17 +0900

    .

--- Arduino.py
+++ Arduino.py
@@ -2,10 +2,15 @@ import serial
 import time
 
 ser = serial.Serial("COM9", 9600) # 아두이노의 포트와 보율을 지정합니다.
-c = input()
-c = c.encode('utf-8')
-ser.write(c)
+try:
+    while(1):
+      c = input()
+      c = c.encode('utf-8')
+      ser.write(c)
 
-time.sleep(1)
-y = ser.readline()
-print(y.decode('utf-8').strip())
+      time.sleep(1)
+      y = ser.readline()
+      print(y.decode('utf-8').strip())
+
+except KeyboardInterrupt:
+    print("중지")
