ad675c7003d28ea0599b34a074b64f2109922f6c
KIM2C1 <119794073+KIM2C1@users.noreply.github.com>
2023-02-15 14:58:44 +0900

    Update keyboard_test.py

--- Python_practice/keyboard_test.py
+++ Python_practice/keyboard_test.py
@@ -4,11 +4,11 @@ import time
 buff = 1
 buff2 = 1
 while buff:
-    if keyboard.is_pressed("up") and buff2 == 1:
+    if keyboard.is_pressed("w") and buff2 == 1:
         print("1")
         time.sleep(0.5)
         buff2 = 0
-    if keyboard.is_pressed("down") and buff2 == 1:
+    if keyboard.is_pressed("s") and buff2 == 1:
         print("2")
         time.sleep(0.5)
         buff2 = 0
