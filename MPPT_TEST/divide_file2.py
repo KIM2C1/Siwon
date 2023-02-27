c37f6eb80daba380ef5939197f61cebf59b2ffe9
KIM2C1 <siwon9572@gmail.com>
2023-02-28 00:14:24 +0900

    .

--- MPPT_TEST/divide_file2.py
+++ MPPT_TEST/divide_file2.py
@@ -1,48 +1,67 @@
 import time
 from divide_file1 import MyStruct
 
-data_C = "220.0 22.7 220.0 60.0 22.7 5000 5000 48.0 48.0 48.0 55.5 55.5 3 40 040 1 0 0 9 01 0 0 54.0 0 1 000 2"
-data_E = "000.0 00.0 221.3 59.9 0087 0046 001 372 53.00 000 086 0036 0000 000.0 00.00 00000 00010000 00 00 00000 010"
-data_F = "0 00000000000000 P 00 000.0 00.00 000.0 00.00 0000 0000 000 00.0 000 000 000.0 000 00000 00000 000 00000000 0 0 000 000 00 00 000"
+QID = "92932111101485"
+QSID = "1492932111101485005535"
+QVFW = "VERFW:00072.00"
+QVFW2 = "VERFW2:00000.00"
+QPIRI = "220.0 22.7 220.0 60.0 22.7 5000 5000 48.0 48.0 48.0 55.5 55.5 3 40 040 1 0 0 9 01 0 0 54.0 0 1 000 2"
+QFLAG = "EbcvxzDajkuy"
+QPIGS = "000.0 00.0 218.8 59.9 0065 0015 001 374 53.20 000 089 0035 0000 000.0 00.00 00000 00010000 00 00 00000 010"
+QPGSn = "0 00000000000000 P 00 000.0 00.00 000.0 00.00 0000 0000 000 00.0 000 000 000.0 000 00000 00000 000 00000000 0 0 000 000 00 00 000"
+QMOD = "B"
+QPIWS = "100001000000000000000000000000000000"
+QDI = "230.0 50.0 0030 42.0 54.0 56.4 46.0 60 1 0 2 0 0 0 0 0 1 1 0 0 1 0 54.0 0 1 000 0"
+QMCHGCR = "010 020 030 040 050 060 070 080 090 100 110 120 130 140"
+QMUCHGCR = "002 010 020 030 040 050 060"
+QOPM = "NAKss"
 
 ##############################################
-data_C_list = list(data_C.split())
+QPIRI_list = list(QPIRI.split())
 for i, key in enumerate(MyStruct.QPIRI):
-    MyStruct.QPIRI[key] = data_C_list[i]
+    MyStruct.QPIRI[key] = QPIRI_list[i]
 
 
-for key, value in MyStruct.QPIRI.items():
-    print(f"{key}: {value}")
+#for key, value in MyStruct.QPIRI.items():
+    #print(f"{key}: {value}")
 
 ###############################################
-data_E_list = list(data_E.split())
+QPIGS_list = list(QPIGS.split())
 for i, key in enumerate(MyStruct.QPIGS):
-    MyStruct.QPIGS[key] = data_E_list[i]
+    MyStruct.QPIGS[key] = QPIGS_list[i]
 
 
-for key, value in MyStruct.QPIGS.items():
-    print(f"{key}: {value}")
+#for key, value in MyStruct.QPIGS.items():
+    #print(f"{key}: {value}")
 
 ###############################################
-data_F_list = list(data_F.split())
+QPGSn_list = list(QPGSn.split())
 for i, key in enumerate(MyStruct.QPGSn):
-    MyStruct.QPGSn[key] = data_F_list[i]
+    MyStruct.QPGSn[key] = QPGSn_list[i]
 
 
-for key, value in MyStruct.QPGSn.items():
-    print(f"{key}: {value}")
+#for key, value in MyStruct.QPGSn.items():
+    #print(f"{key}: {value}")
 
 ###############################################
+QDI_list = list(QDI.split())
+for i, key in enumerate(MyStruct.QDI):
+    MyStruct.QDI[key] = QDI_list[i]
 
+
+#for key, value in MyStruct.QDI.items():
+    #print(f"{key}: {value}")
+
+###############################################
 ## QFLAG Divide
 data_D = 'EbcvxzDajkuy'
 exclude_chars = ['E', 'D']
-data_D_list_E = []
-data_D_list_D = []
+QFLAG_E = []
+QFLAG_D = []
 i = 0
 temp = 0
 
-while i < len(data_D):
+while i < len(QFLAG):
     if data_D[i] == 'E':
         temp = 0
         i += 1
@@ -51,11 +70,24 @@ while i < len(data_D):
         i += 1
     
     if temp == 0:
-        data_D_list_E.append(data_D[i])
+        QFLAG_E.append(QFLAG[i])
     elif temp == 1:
-        data_D_list_D.append(data_D[i])
+        QFLAG_D.append(QFLAG[i])
     i += 1
 
-print(data_D_list_E)
-print(data_D_list_D) 
+def dic(data):
+    for key, value in MyStruct.data.items():
+            print(f"{key}: {value}")
 
+
+ex_data = 'QPIRI', 'QPIGS', 'QPGSn', 'QDI'
+
+while True:
+    find_command = input("Select(Out is ESC): ")
+    if find_command == 'QPIRI' and 'QPIGS' and 'QPGSn' and 'QDI':
+        dic(find_command)
+    elif find_command == 'QFLAG':
+        print(QFLAG_E)
+        print(QFLAG_D) 
+    else:
+        print(find_command)
