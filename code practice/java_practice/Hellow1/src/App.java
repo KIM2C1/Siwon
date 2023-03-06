5b39168f337cd78566ef68eeb895df1b3a240772
KIM2C1 <siwon9572@gmail.com>
2023-03-07 00:47:46 +0900

    Update App.java

--- code practice/java_practice/Hellow1/src/App.java
+++ code practice/java_practice/Hellow1/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World1!");
+
+        int A = 10;
+        System.out.println(A);
+
     }
 }
