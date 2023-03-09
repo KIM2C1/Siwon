e986967e3dd7123bc2ca16de0df3d94c57345b8e
KIM2C1 <119794073+KIM2C1@users.noreply.github.com>
2023-03-09 18:57:37 +0900

    Update drive-test10.cpp

--- LAB/drive-test10.cpp
+++ LAB/drive-test10.cpp
@@ -43,14 +43,14 @@ int OUT_BR_1 = LOW;
 int OUT_BR_2 = LOW;
 
  void state_value(string state) {
-    if (state == 'ready') {
+    if (state == "ready") {
       OUT_FL_1 = OUT_FL_2 = OUT_FR_1 = OUT_FR_2 = OUT_BL_1 =  OUT_BL_2 = OUT_BR_1 = OUT_BR_2 = LOW;
       softPwmWrite(EN_FL, 0);
       softPwmWrite(EN_BL, 0);
       softPwmWrite(EN_FR, 0);
       softPwmWrite(EN_BR, 0);
     }
-    else if (state == 'go') {
+    else if (state == "go") {
       OUT_FL_1 = OUT_FR_2 = OUT_BL_1 = OUT_BR_2 = LOW;
       OUT_FL_2 = OUT_FR_1 = OUT_BL_2 = OUT_BR_1 = HIGH;
       softPwmWrite(EN_FL, 100);
@@ -58,7 +58,7 @@ int OUT_BR_2 = LOW;
       softPwmWrite(EN_FR, 100);
       softPwmWrite(EN_BR, 100);
     }
-    else if (state == 'back') {
+    else if (state == "back") {
       OUT_FL_1 = OUT_FR_2 = OUT_BL_1 = OUT_BR_2 = HIGH;
       OUT_FL_2 = OUT_FR_1 = OUT_BL_2 = OUT_BR_1 = LOW;
       softPwmWrite(EN_FL, 100);
@@ -66,7 +66,7 @@ int OUT_BR_2 = LOW;
       softPwmWrite(EN_FR, 100);
       softPwmWrite(EN_BR, 100);
     }
-    else if (state == 'right') {
+    else if (state == "right") {
       OUT_FL_2 = OUT_FR_2 = OUT_BL_1 = OUT_BR_1 = HIGH;
       OUT_FL_1 = OUT_FR_1 = OUT_BL_2 = OUT_BR_2 = LOW;
       softPwmWrite(EN_FL, 100);
@@ -74,7 +74,7 @@ int OUT_BR_2 = LOW;
       softPwmWrite(EN_FR, 100);
       softPwmWrite(EN_BR, 100);
     }
-    else if (state == 'left') {
+    else if (state == "left") {
       OUT_FL_1 = OUT_FR_1 = OUT_BL_2 = OUT_BR_2 = HIGH;
       OUT_FL_2 = OUT_FR_2 = OUT_BL_1 = OUT_BR_1 = LOW;
       softPwmWrite(EN_FL, 100);
