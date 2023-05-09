c1bece5649468885f1149edbd12051810d889d4d
KIM2C1 <119794073+KIM2C1@users.noreply.github.com>
2023-05-09 18:11:40 +0900

    Update PID_mt.py

--- test folder/PID_mt.py
+++ test folder/PID_mt.py
@@ -9,7 +9,7 @@ IN_FL_2 = 5
 GPIO.setup(EN_FL, GPIO.OUT)
 GPIO.setup(IN_FL_1, GPIO.OUT)
 GPIO.setup(IN_FL_2, GPIO.OUT)
-pwm_FL = GPIO.PWM(EN_FL,255)
+pwm_FL = GPIO.PWM(EN_FL,100)
 pwm_FL.start(0)
 OUT_FL_1 = 0
 OUT_FL_2 = 0
