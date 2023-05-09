9bae5dd0c7461dc1369424aa741b4fdb40f767ff
KIM2C1 <119794073+KIM2C1@users.noreply.github.com>
2023-05-09 18:08:34 +0900

    Update PID_mt.py

--- test folder/PID_mt.py
+++ test folder/PID_mt.py
@@ -9,7 +9,7 @@ IN_FL_2 = 5
 GPIO.setup(EN_FL, GPIO.OUT)
 GPIO.setup(IN_FL_1, GPIO.OUT)
 GPIO.setup(IN_FL_2, GPIO.OUT)
-pwm_FL = GPIO.PWM(EN_FL,100)
+pwm_FL = GPIO.PWM(EN_FL,255)
 pwm_FL.start(0)
 OUT_FL_1 = 0
 OUT_FL_2 = 0
@@ -23,4 +23,4 @@ GPIO.output(IN_FL_1,OUT_FL_1)
 GPIO.output(IN_FL_2,OUT_FL_2)
 
 while (True):
-    pwm_FL.ChangeDutyCycle(100)
+    pwm_FL.ChangeDutyCycle(255)
