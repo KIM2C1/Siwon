aa80904fe92c9c887644acca18744d8b1489a751
KIM2C1 <119794073+KIM2C1@users.noreply.github.com>
2023-05-13 17:42:45 +0900

    Update moter_purse.cpp

--- LAB/moter_purse.cpp
+++ LAB/moter_purse.cpp
@@ -2,7 +2,7 @@
 #include <softPwm.h>
 #include <iostream>
 
-#define pwmPinA 25             // 모터드라이버 ENA - GPIO핀 번호: 12
+#define pwmPinA 25             // 모터드라이d버 ENA - GPIO핀 번호: 12
 #define AIN1 24            // IN1 - GPIO핀 번호: 16
 #define AIN2 23            // IN2 - GPIO핀 번호 : 25 
 #define encPinA 22           // 보라색 (A) - GPIO핀 번호 : 23
