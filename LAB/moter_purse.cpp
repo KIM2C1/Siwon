83950b117fc4c679de0d38961c4615ce8d8e5714
KIM2C1 <119794073+KIM2C1@users.noreply.github.com>
2023-05-17 12:38:58 +0900

    Update moter_purse.cpp

--- LAB/moter_purse.cpp
+++ LAB/moter_purse.cpp
@@ -5,16 +5,23 @@
 #define pwmPinA 25             // 모터드라이d버 ENA - GPIO핀 번호: 12
 #define AIN1 24            // IN1 - GPIO핀 번호: 16
 #define AIN2 23            // IN2 - GPIO핀 번호 : 25 
-#define encPinA 22           // 보라색 (A) - GPIO핀 번호 : 23
-#define encPinB 21           // 파랑색 (B) - GPIO핀 번호 : 24
+#define encPinA 29           // 보라색 (A) - GPIO핀 번호 : 23
+#define encPinB 28           // 파랑색 (B) - GPIO핀 번호 : 24
 
 volatile int pulse_count = 0;
 
+void pulse_callback_A() {
+    //pulse_count += 1;
+    std::cout<<"pulse_A_working!"<<std::endl;
+    delay(50);
+    std::cout<<"delay_A end!"<<std::endl;
+}
+
 void pulse_callback_B() {
     //pulse_count += 1;
-    std::cout<<"pulse_working!"<<std::endl;
-    delay(1000);
-    std::cout<<"delay end!"<<std::endl;
+    std::cout<<"pulse_B_working!"<<std::endl;
+    delay(50);
+    std::cout<<"delay_B end!"<<std::endl;
 }
 
 int main() {
@@ -35,7 +42,8 @@ int main() {
     digitalWrite(AIN2, HIGH);
     
     //wiringPiISR(29, INT_EDGE_BOTH, &pulse_callback);
-    wiringPiISR(29, INT_EDGE_BOTH, &pulse_callback_B);
+    wiringPiISR(encPinA, INT_EDGE_BOTH, &pulse_callback_A);
+    wiringPiISR(encPinB, INT_EDGE_BOTH, &pulse_callback_B);
     while (1) {
     }
 
