b48cc9282ec22139888c8cdd1a3448bfbf538dab
KIM2C1 <119794073+KIM2C1@users.noreply.github.com>
2023-06-25 14:18:52 +0900

    .

--- MPPT_TEST/date_read.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-ser = serial.Serial('COM11', 9600)
-
-while(1):
-    data = ser.read()
-    print(data)
-
-ser.close()
--- send_data.py
+++
@@ -1,7 +0,0 @@
-def custom_function():
-    i = 0
-    while(i != 100):
-        i += 1
-    return i
-
-custom_function.counter = 0
